d3b4826f5e608b2c07795d4efa6a19afe06e5429
SundaramDubey <43514004+SundaramDubey@users.noreply.github.com>
2019-04-21 18:01:49 +0530

    want halp .java file added

---
+++ call us or need.java
@@ -0,0 +1,71 @@
+package com.bignerdranch.android.splash;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * Created by user on 8/10/16.
+ */
+public class CallUs extends AppCompatActivity {
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.best);
+
+        String name;
+
+
+        int num;
+        // Find the ListView resource.
+        mainListView = (ListView) findViewById( R.id.listView );
+
+        // Create and populate a List of planet names.
+        String[] planets = new String[] {
+                new String("Ravi    :  +919561720764"),
+                new String("Aniket  :  +919561720764"),
+                new String("Shri    :  +919561720764"),
+                new String("Siddhant:  +919561720764"),
+                new String("Vishal  :  +919561720764"),
+                new String("Tanmay  :  +919561720764"),};
+
+        // Create ArrayAdapter using the planet list.
+        // Set the ArrayAdapter as the ListView's adapter.
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, planets);
+
+        mainListView.setAdapter(adapter);
+
+        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String item = ((TextView)view).getText().toString();
+                if(position==0)
+                    setContentView(R.layout.ravi);
+                else if(position==1)
+                    setContentView(R.layout.ani);
+                else if(position==2)
+                    setContentView(R.layout.shri);
+                else if(position==3)
+                    setContentView(R.layout.sid);
+                else if(position==4)
+                    setContentView(R.layout.vish);
+                else if(position==5)
+                    setContentView(R.layout.tan);
+            }
+        });
+    }
+
+
+
+}
